f7834f44dc10b49ebb3b43a5c8a3c0b04d2204cd
Luka Maisuradze <codeverselm@gmail.com>
2025-06-29 20:11:13 +0400

    attempt n2

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -10,41 +10,56 @@ export async function POST(request: Request) {
 
   const ai = new GoogleGenAI({});
 
-  console.log(role, level, techstack, type, amount, userid);
+  console.log(
+    "role",
+    role,
+    "level",
+    level,
+    "techstack",
+    techstack,
+    "type",
+    type,
+    "amount",
+    amount
+  );
 
   try {
-    // const response = await ai.models.generateContent({
-    //   model: "gemini-2.5-flash",
-    //   contents: `Prepare questions for a job interview.
-    //     The job role is ${role}.
-    //     The job experience level is ${level}.
-    //     The tech stack used in the job is: ${techstack}.
-    //     The focus between behavioural and technical questions should lean towards: ${type}.
-    //     The amount of questions required is: ${amount}.
-    //     Please return only the questions, without any additional text.
-    //     The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
-    //     Return the questions formatted like this:
-    //     ["Question 1", "Question 2", "Question 3"]
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
 
-    //     Thank you! <3
-    // `,
-    // });
+        Thank you! <3
+    `,
+    });
 
-    // const questions = response.text?.split(",");
+    console.log("response", response);
 
-    // const interview = {
-    //   role: role,
-    //   type: type,
-    //   level: level,
-    //   techstack: techstack.split(","),
-    //   questions: questions,
-    //   userId: userid,
-    //   finalized: true,
-    //   // coverImage: getRandomInterviewCover(),
-    //   createdAt: new Date().toISOString(),
-    // };
+    const questions = response.text?.split(",");
 
-    // await db.collection("interviews").add(interview);
+    const interview = {
+      role: role,
+      type: type,
+      level: level,
+      techstack: techstack.split(","),
+      questions: questions,
+      userId: userid,
+      finalized: true,
+      // coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    console.log("interview", interview);
+
+    await db.collection("interviews").add(interview);
 
     return Response.json(
       { success: true, message: "success" },
